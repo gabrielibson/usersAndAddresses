a8e4362501bf6416d32ef5e3b3e8475d4425d649
Gabriel Ibson <gabrielibson@gmail.com>
2021-05-31 19:54:47 -0300

    Add via cep client

---
+++ src/main/java/com/orange/talents/users/client/Endereco.java
@@ -0,0 +1,13 @@
+package com.orange.talents.users.client;
+
+import lombok.Getter;
+
+@Getter
+public class Endereco {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+}
---
+++ src/main/java/com/orange/talents/users/client/ViaCepClient.java
@@ -0,0 +1,13 @@
+package com.orange.talents.users.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "https://viacep.com.br/ws/", name = "viacep")
+public interface ViaCepClient {
+
+    @GetMapping("{cep}/json/")
+    Endereco findByCep(@PathVariable String cep);
+
+}
