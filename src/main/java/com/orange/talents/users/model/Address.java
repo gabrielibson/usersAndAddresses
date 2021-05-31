9c0ed79105027c34cbb86a0945b64fea1dad55ad
Gabriel Ibson <gabrielibson@gmail.com>
2021-05-31 19:37:14 -0300

    Add entities

---
+++ src/main/java/com/orange/talents/users/model/Address.java
@@ -0,0 +1,29 @@
+package com.orange.talents.users.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+public class Address {
+    public Address() {}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private Integer number;
+    private String complement;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private String cep;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    @Setter
+    private User user;
+}
---
+++ src/main/java/com/orange/talents/users/model/User.java
@@ -0,0 +1,38 @@
+package com.orange.talents.users.model;
+
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+public class User {
+    public User() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @CPF
+    @Column(unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @Builder.Default
+    private final List<Address> addresses = new ArrayList<>();
+}
